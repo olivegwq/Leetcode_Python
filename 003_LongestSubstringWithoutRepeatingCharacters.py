4203ba52b010900badb3184547fa0e9a570ecd59
olivegwq <olivegwq@gmail.com>
2015-10-15 23:14:33 -0700

    Update 003_LongestSubstringWithoutRepeatingCharacters.py

--- 003_LongestSubstringWithoutRepeatingCharacters.py
+++ 003_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,21 +1,22 @@
 '''
 Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 '''
-class Solution:
-    # @return an integer
+class Solution(object):
     def lengthOfLongestSubstring(self, s):
- """
+        """
         :type s: str
         :rtype: int
         """
-        if not len(s):
-            return 0
-        head, end =0
+        if len(s)<2:
+            return len(s)
+        head, end, strl =0,1,1
         for i in range(1,len(s)):
             indx=s[head:end].find(s[i])
             if indx==-1:
-                end=i
+                end=i+1
+                strl=max(strl,end-head)
             else:
-                head=indx
-        return len(s[head:end])+1      
+                head=head+indx+1
+                end=i+1
+        return strl      
                 
