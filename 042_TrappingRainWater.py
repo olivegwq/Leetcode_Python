e90c748a7d2b250435a3fd472576193471c9885b
olivegwq <olivegwq@gmail.com>
2015-10-19 00:52:16 -0700

    Update 042_TrappingRainWater.py

--- 042_TrappingRainWater.py
+++ 042_TrappingRainWater.py
@@ -1,1 +1,25 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if len(height)<3:
+            return 0
+        
+        anchor=height.index(max(height))
+        
+        #left
+        indx1,cur_h,vol_left=0,0,0
 
+        while indx1<anchor:
+            cur_h=max(cur_h,height[indx1])
+            vol_left+=(cur_h-height[indx1])*1
+            indx1+=1
+        #right
+        indx2,cur_h,vol_right=len(height)-1,0,0
+        while indx2>anchor:
+            cur_h=max(cur_h,height[indx2])
+            vol_right+=(cur_h-height[indx2])*1
+            indx2-=1
+        return vol_left+vol_right
