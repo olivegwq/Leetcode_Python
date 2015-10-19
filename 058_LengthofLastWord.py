fc345b76713578da51e1177c5e8147505cb77cdd
olivegwq <olivegwq@gmail.com>
2015-10-19 01:07:42 -0700

    Update 058_LengthofLastWord.py

--- 058_LengthofLastWord.py
+++ 058_LengthofLastWord.py
@@ -1,1 +1,16 @@
-
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not len(s):
+            return 0
+        indx=len(s)-1
+        while s[indx]==' ':
+            indx-=1
+            if indx<0:
+                return 0
+        indx2=s[:indx].rfind(' ')
+        
+        return indx-indx2
