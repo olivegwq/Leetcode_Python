d4a9b5b7ad76789d73e7711d59e440c5094c4ef6
olivegwq <olivegwq@gmail.com>
2015-10-20 15:31:30 -0700

    Update 168_Excel Sheet Column Title.py

--- 168_Excel Sheet Column Title.py
+++ 168_Excel Sheet Column Title.py
@@ -1,1 +1,13 @@
-
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        col=''
+      
+        while n:
+            col=chr((n-1)%26+ord('A'))+col
+            n=(n-(n-1)%26)/26
+        
+        return col
