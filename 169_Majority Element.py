0f1045b9103fdc978c22e24d5d2714858c8b3910
olivegwq <olivegwq@gmail.com>
2015-10-20 15:31:09 -0700

    Update 169_Majority Element.py

--- 169_Majority Element.py
+++ 169_Majority Element.py
@@ -1,1 +1,20 @@
 
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dict={}
+        maj=[]
+        n=len(nums)//2
+        for item in nums:
+            if item in dict:
+                dict[item]+=1
+                if dict[item]>n:
+                    return item
+            else:
+                dict[item]=1
+                if dict[item]>n:
+                    return item
+        return 0
