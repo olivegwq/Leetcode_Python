ffe40de1ad2fe01a65d4bb5d529cb0ef5b281cba
olivegwq <olivegwq@gmail.com>
2015-10-20 00:57:28 -0700

    Create 125_ValidPalidrom.py

---
+++ 125_ValidPalidrom.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s=[ch.lower() for ch in s if ch.isalnum()]
+        
+        return s==s[::-1]
