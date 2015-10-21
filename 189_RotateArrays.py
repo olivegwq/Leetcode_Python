0a8f00fa9df89be118b48909eb34dd370c24f9a9
olivegwq <olivegwq@gmail.com>
2015-10-20 20:49:09 -0700

    Update 189_RotateArrays.py

--- 189_RotateArrays.py
+++ 189_RotateArrays.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if k==0 or not nums:
+            return
+        k%=len(nums)
+        nums[:]=nums[-k:]+nums[0:-k]
+        return
