5cdbefd7902c3dded17e236e4e73b2cb5ac9366b
olivegwq <olivegwq@gmail.com>
2015-10-21 15:32:27 -0700

    Update 231_PowerofTwo.py

--- 231_PowerofTwo.py
+++ 231_PowerofTwo.py
@@ -1,1 +1,12 @@
-
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n==1:
+            return True
+        elif n%2 or n<=0:
+            return False
+        else:
+            return self.isPowerOfTwo(n/2)
