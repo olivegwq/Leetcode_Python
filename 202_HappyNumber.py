9d1535832c7ded754455e5a08dc2bbd31406a2be
olivegwq <olivegwq@gmail.com>
2015-10-21 00:02:38 -0700

    Update 202_HappyNumber.py

--- 202_HappyNumber.py
+++ 202_HappyNumber.py
@@ -1,1 +1,20 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        dict={}
 
+        while True:
+            sum=0
+            while n:
+                sum+=math.pow(n%10,2)
+                n//=10
+            if sum==1:
+                return True
+            elif sum in dict:
+                return False
+            else:
+                dict[sum]=1
+                n=sum
