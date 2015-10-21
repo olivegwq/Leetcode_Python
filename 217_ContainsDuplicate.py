b8d4801b731f822069dd46b46a811d0083d8a57f
olivegwq <olivegwq@gmail.com>
2015-10-21 03:40:21 -0700

    Update 217_ContainsDuplicate.py

--- 217_ContainsDuplicate.py
+++ 217_ContainsDuplicate.py
@@ -1,1 +1,15 @@
-
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if not len(nums):
+            return False
+        pool=set()
+        for i in range(len(nums)):
+            if nums[i] in pool:
+                return True
+            else:
+                pool.add(nums[i])
+        return False
