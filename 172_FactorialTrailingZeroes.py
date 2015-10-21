87501bddeb8c43a9d4bad09c6ed2fb2756eaff9d
olivegwq <olivegwq@gmail.com>
2015-10-20 20:03:20 -0700

    Update 172_FactorialTrailingZeroes.py

--- 172_FactorialTrailingZeroes.py
+++ 172_FactorialTrailingZeroes.py
@@ -1,1 +1,11 @@
-
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        count=0
+        while n:
+            count+=n//5
+            n=n//5
+        return count
