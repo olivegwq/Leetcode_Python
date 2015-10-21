301ad4cb98d4889ecdad4d7a1a4241c295c80e33
olivegwq <olivegwq@gmail.com>
2015-10-20 21:05:55 -0700

    Update 190_ReverseBits.py

--- 190_ReverseBits.py
+++ 190_ReverseBits.py
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def reverseBits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res,i,=0,0
+        while n:
+            res=res*2+n%2
+            n//=2
+            i+=1
+        if i<32:
+            res=res*math.pow(2,32-i)
+        return int(res)
