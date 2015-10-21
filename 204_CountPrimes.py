095f9a37c3de5f0c2b4b5d9050e427d49a1bb92d
olivegwq <olivegwq@gmail.com>
2015-10-21 02:27:14 -0700

    Update 204_CountPrimes.py

--- 204_CountPrimes.py
+++ 204_CountPrimes.py
@@ -1,1 +1,15 @@
-
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n<3:
+            return 0
+        pool=[0,0]+[1 for i in range(2,n)]
+        
+        for i in range(2,n):
+            if pool[i]:
+                for j in range(2,(n-1)/i+1):
+                    pool[j*i]=0
+        return sum(pool)
