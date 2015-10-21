fdbb286f5fc7ee94caa1c4399f38724a147ec260
olivegwq <olivegwq@gmail.com>
2015-10-21 14:21:00 -0700

    Update 219_ContainsDuplicateII.py

--- 219_ContainsDuplicateII.py
+++ 219_ContainsDuplicateII.py
@@ -1,1 +1,21 @@
-
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        if not nums:
+            return False
+        
+        dict={}
+        
+        for i in range(len(nums)):
+            if nums[i] in dict:
+                if abs(i-min(dict[nums[i]]))<=k or abs(i-max(dict[nums[i]]))<=k:
+                    return True
+                else:
+                    dict[nums[i]].append(i)
+            else:
+                dict[nums[i]]=[i]
+        return False
