a241fce372f07c76c4b0272d18aba76caf66832a
olivegwq <olivegwq@gmail.com>
2015-10-21 03:19:23 -0700

    Update 205_IsomorphicStrings.py

--- 205_IsomorphicStrings.py
+++ 205_IsomorphicStrings.py
@@ -1,1 +1,21 @@
-
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        dicts,dictt={},{}
+        for i in range(len(s)):
+            if s[i] not in dicts:
+                dicts[s[i]]=t[i]
+            elif dicts[s[i]]!=t[i]:
+                return False
+            if t[i] not in dictt:
+                dictt[t[i]]=s[i]
+            elif dictt[t[i]]!=s[i]:
+                return False
+        return True
+                
