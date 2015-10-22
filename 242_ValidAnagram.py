da5606a7d84c39787813afb18624b4b1347afaae
olivegwq <olivegwq@gmail.com>
2015-10-21 21:26:37 -0700

    Update 242_ValidAnagram.py

--- 242_ValidAnagram.py
+++ 242_ValidAnagram.py
@@ -1,1 +1,26 @@
-
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        dict={}
+        
+        for i in s:
+            if i in dict:
+                dict[i]+=1
+            else:
+                dict[i]=1
+        for j in t:
+            if j not in dict:
+                return False
+            elif j in dict:
+                if dict[j]==1:
+                    del dict[j]
+                else:
+                    dict[j]-=1
+        if len(dict):
+            return False
+        else:
+            return True
