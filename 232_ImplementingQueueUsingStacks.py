94f8e33969ad123d68e1487145ff8079e27e7059
olivegwq <olivegwq@gmail.com>
2015-10-21 20:17:10 -0700

    Update 232_ImplementingQueueUsingStacks.py

--- 232_ImplementingQueueUsingStacks.py
+++ 232_ImplementingQueueUsingStacks.py
@@ -1,1 +1,39 @@
+class Queue(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.s1,self.s2=[],[]
 
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        if not self.s1:
+            self.s1.append(x)
+        else:
+            self.s2.append(x)
+    
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        self.s1.pop()
+        
+        while len(self.s2)>1:
+            self.s1.append(self.s2.pop())
+        self.s1,self.s2=self.s2,self.s1
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        return self.s1[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return not self.s1
