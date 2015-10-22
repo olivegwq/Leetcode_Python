6bad43e43f4235e7b111c33cb9a43c16ff251181
olivegwq <olivegwq@gmail.com>
2015-10-22 15:00:32 -0700

    Update 283_MoveZeros.py

--- 283_MoveZeros.py
+++ 283_MoveZeros.py
@@ -1,1 +1,25 @@
-
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return nums
+        
+        pre,pos=0,0
+        
+        while pre<len(nums) and pos<len(nums):
+            while nums[pre]:
+                pre+=1
+                if pre==len(nums):
+                    return
+            pos=pre
+            while not nums[pos]:
+                pos+=1
+                if pos==len(nums):
+                    return
+            nums[pre],nums[pos]=nums[pos],nums[pre]
+                
+            pre+=1
+        return
