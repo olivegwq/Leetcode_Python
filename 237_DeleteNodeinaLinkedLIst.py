1432c598388cd699b3d99f3bfd3ff89448505b2b
olivegwq <olivegwq@gmail.com>
2015-10-21 21:20:13 -0700

    Update 237_DeleteNodeinaLinkedLIst.py

--- 237_DeleteNodeinaLinkedLIst.py
+++ 237_DeleteNodeinaLinkedLIst.py
@@ -1,1 +1,10 @@
-
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if not node:
+            return
+        node.val=node.next.val
+        node.next=node.next.next
