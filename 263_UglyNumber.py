7077c3bdc8b288564d2e57fa5cc97279389060f9
olivegwq <olivegwq@gmail.com>
2015-10-22 23:48:28 -0700

    Update 263_UglyNumber.py

--- 263_UglyNumber.py
+++ 263_UglyNumber.py
@@ -1,1 +1,19 @@
-
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+   
+        if num<=0:
+            return False
+        while not num%2:
+            num=num/2
+        while not num%5:
+            num=num/5
+        while not num%3:
+            num=num/3
+        if num==1or num==-1:
+            return True
+        else:
+            return False
